97f7bf55487d43133536b707d474275ef8858303
nazreen <nxmohamad@gmail.com>
2024-04-06 14:33:03 +0800

    trim

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,59 +1,6 @@
 import type { Context, Config } from "@netlify/functions"
 import { Network, Alchemy } from "alchemy-sdk";
 
-interface CacheBalanceEntry {
-    timestamp: number;
-    balance: string;
-}
-
-interface Cache {
-    [address: string]: {
-        [network: string]: CacheBalanceEntry
-    }
-}
-
-const cache: Cache = {};
-
-function readFromCache(address: string, network: string): CacheBalanceEntry | undefined {
-    return cache[address]?.[network];
-}
-
-function writeToCache(address: string, network: string, balance: string) {
-    if (!cache[address]) {
-        cache[address] = {};
-    }
-    cache[address][network] = {
-        timestamp: Date.now(),
-        balance
-    };
-}
-
-async function getBalanceWithCache(address: string, network: string): Promise<string> {
-    const now = Date.now();
-    const oneMinute = 60000; // 60,000 milliseconds
-    const cacheEntry = readFromCache(address, network);
-    const cacheValid = cacheEntry && (now - cacheEntry.timestamp < oneMinute && cache.data);
-    // Check if the cache is valid
-    if (cacheValid) {
-        console.log("Using cached data");
-        return cacheEntry.balance; // Return cached data if it's less than a minute old
-    } else {
-        console.log("Fetching new data");
-        // Fetch new data
-        const balance = await getBalance(address, network);
-
-        if (!balance) {
-            throw new Error("Failed to fetch balance");
-        }
-
-        writeToCache(address, network, balance);
-
-        return balance;
-    }
-}
-
-
-
 type AlchemyClients = {
     [network in Network]: Alchemy;
 };
@@ -77,13 +24,6 @@ interface Balances {
 
 const address = "0x3a92924f2c8aAA64E7AEF846f73C0463A2f54173";
 
-async function getBalance(address: string, network: string): Promise<string | undefined> {
-    const client = alchemyClients[network];
-    if (!client) return;
-    const rawBalance = await client.core.getBalance(address); // Assuming getBalance returns a Promise
-    const formattedBalance = rawBalance.toString(); // Assuming formatEther takes the balance and formats it
-    return formattedBalance
-}
 
 async function getBalances(): Promise<Balances> {
     const networks = Object.keys(alchemyClients) as Array<keyof typeof alchemyClients>;
